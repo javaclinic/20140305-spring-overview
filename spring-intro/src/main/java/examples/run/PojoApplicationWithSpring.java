e0f6f1e5ca7e11347e2a1861cec18cfcc37361da
Neven Cvetkovic <neven.cvetkovic@gmail.com>
2014-03-02 16:07:09 -0500

    Adding Spring example.

---
+++ spring-intro/src/main/java/examples/run/PojoApplicationWithSpring.java
@@ -0,0 +1,24 @@
+package examples.run;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import examples.config.NoSpringFactory;
+import examples.services.AccountManager;
+
+public class PojoApplicationWithSpring {
+
+	public static void main(String[] args) {
+		
+		BeanFactory factory = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		AccountManager service = factory.getBean("accountManager", AccountManager.class);
+		
+		service.addAccount("0000-0001", "John Doe", 1000.0);
+		service.addAccount("0000-0002", "John Doe", 2000.0);
+		service.addAccount("0000-0003", "Jane Doe", 3000.0);
+		service.addAccount("0000-0004", "Jill Doe", 4000.0);
+			
+	}
+
+}
