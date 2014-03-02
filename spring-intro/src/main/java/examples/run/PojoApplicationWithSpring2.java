5632a0e9f7c484916beb692cd63c4eaaf2a4d58b
Neven Cvetkovic <neven.cvetkovic@gmail.com>
2014-03-02 16:40:44 -0500

    Added spring example with autowiring.

---
+++ spring-intro/src/main/java/examples/run/PojoApplicationWithSpring2.java
@@ -0,0 +1,23 @@
+package examples.run;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import examples.services.AccountManager;
+
+public class PojoApplicationWithSpring2 {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext factory = new ClassPathXmlApplicationContext("applicationContext2.xml");
+		
+		AccountManager service = factory.getBean("accountManager", AccountManager.class);
+		
+		service.addAccount("0000-0001", "John Doe", 1000.0);
+		service.addAccount("0000-0002", "John Doe", 2000.0);
+		service.addAccount("0000-0003", "Jane Doe", 3000.0);
+		service.addAccount("0000-0004", "Jill Doe", 4000.0);
+			
+	}
+
+}
