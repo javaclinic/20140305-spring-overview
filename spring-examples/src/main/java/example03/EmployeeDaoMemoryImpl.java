5807895eac3ef3fc5caf084e396ef587503e723a
Neven Cvetkovic <neven.cvetkovic@gmail.com>
2014-03-03 07:30:01 -0500

    Updated memory implementation not to use static Map. Every time we instantiate impl, it will have its own "database".

--- spring-examples/src/main/java/example03/EmployeeDaoMemoryImpl.java
+++ spring-examples/src/main/java/example03/EmployeeDaoMemoryImpl.java
@@ -13,18 +13,15 @@ import org.springframework.stereotype.Repository;
 public class EmployeeDaoMemoryImpl implements EmployeeDao {
 	
 	private static final Logger LOGGER = Logger.getLogger(EmployeeDaoMemoryImpl.class.getCanonicalName());
-	private static Map<String,Employee> database = new HashMap<>();
-	
-	static {
+	private Map<String,Employee> database = new HashMap<>();
+		
+	public EmployeeDaoMemoryImpl() {
+		LOGGER.info("Inside " + EmployeeDaoMemoryImpl.class.getSimpleName() + "() default constructor. Instantiating dao.");
 		database.put("0001", new Employee("0001", "John Doe", "john@email.com"));
 		database.put("0002", new Employee("0002", "Jane Doe", "jane@email.com"));
 		database.put("0003", new Employee("0003", "Jack Doe", "jack@email.com"));
 		database.put("0004", new Employee("0004", "Jill Doe", "jill@email.com"));
 		database.put("0005", new Employee("0005", "Jenn Doe", "jenn@email.com"));
-	}
-	
-	public EmployeeDaoMemoryImpl() {
-		LOGGER.info("Inside " + EmployeeDaoMemoryImpl.class.getSimpleName() + "() default constructor. Instantiating dao.");
 	}
 	
 	public void saveEmployee(Employee e) {
