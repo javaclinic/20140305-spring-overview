4b3484919e94b9ccfde39d7f8f58f9568ab8b0e9
Neven Cvetkovic <neven.cvetkovic@gmail.com>
2014-03-03 07:06:56 -0500

    Added same database schema i seed data to tests.

---
+++ spring-examples/src/test/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO employees (id,name,email) VALUES ('0001', 'John Doe', 'john@email.com');
+INSERT INTO employees (id,name,email) VALUES ('0002', 'Jane Doe', 'jane@email.com');
+INSERT INTO employees (id,name,email) VALUES ('0003', 'Jack Doe', 'jack@email.com');
+INSERT INTO employees (id,name,email) VALUES ('0004', 'Jill Doe', 'jill@email.com');
+INSERT INTO employees (id,name,email) VALUES ('0005', 'Jenn Doe', 'jenn@email.com');
+
+INSERT INTO phonebook (name,number,note) VALUES ('John Doe', '415-555-1111', 'Home');
+INSERT INTO phonebook (name,number,note) VALUES ('John Doe', '415-555-2222', 'Cell');
+INSERT INTO phonebook (name,number,note) VALUES ('Jane Doe', '415-555-3333', 'Home');
+INSERT INTO phonebook (name,number,note) VALUES ('Jane Doe', '415-555-4444', 'Work');
+INSERT INTO phonebook (name,number,note) VALUES ('Jane Doe', '415-555-5555', 'Fax');
